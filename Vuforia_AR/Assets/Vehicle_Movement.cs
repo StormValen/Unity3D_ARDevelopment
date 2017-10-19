d1c8b1853397e56ff5f658c6bb87ff656473b0c2
StormValen <valentin.g.l@gmail.com>
2017-10-20 01:31:50 +0200

    HUD

---
+++ Vuforia_AR/Assets/Vehicle_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle_Movement : MonoBehaviour {
+
+	public GameObject car;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goForward(){
+		Vector3 mov = new Vector3 (0.2f, 0.0f, 0.0f);
+		car.transform.Translate (mov*Time.deltaTime);
+	}
+}
