3db8c873d2d4633b149f1741d5182fc359a254c9
StormValen <valentin.g.l@gmail.com>
2018-01-08 14:21:08 +0100

    Final

---
+++ Vuforia_AR/Assets/Black Dragon/PlayAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+    public Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim.Play("Armature|Fly_New");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
